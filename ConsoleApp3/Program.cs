1edda0d7fd5770125471f7e52daea284a2ea5365
dev <dev@A34PC12>
2024-09-13 12:28:34 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("з");
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.WriteLine("днем");
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("народження!");
