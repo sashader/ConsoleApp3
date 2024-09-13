4bcaf08d4429faba428841456266ce6c8ab91a82
sashader <sashader1308@gmail.com>
2024-09-13 12:48:59 +0300

    додано новий рядок

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,8 +1,15 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.OutputEncoding = System.Text.Encoding.UTF8;
 Console.ForegroundColor = ConsoleColor.Yellow;
 Console.WriteLine("з");
 Console.ForegroundColor = ConsoleColor.Blue;
 Console.WriteLine("днем");
 Console.ForegroundColor = ConsoleColor.Red;
 Console.WriteLine("народження!");
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("Ще не вмерла Україна, і слава, і воля");
+Console.ResetColor();   
